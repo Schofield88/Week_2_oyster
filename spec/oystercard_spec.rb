49ccd5c301eb3c368ffc86d17d79dea55c75c04d
Luke Phyall <lukeprospect@yahoo.com>
2019-02-11 14:53:57 +0000

    Can now top up with value

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,4 +8,13 @@ describe Oystercard do
     expect(card.balance).to eq(0)
   end
 
+  # it { is_expected.to respond_to :top_up }
+
+  # it { is_expected.to respond_to(:top_up).with(1).argument }
+
+  it "will top_up card with value" do
+    expect(card.top_up(50)).to eq(50)
+  end
+
+
 end
